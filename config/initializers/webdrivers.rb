5df8197ca2bf83d1e5c3e76c8330ade72cdd5b9e
Harsh bawniya <habawniya@bestpeers.in>
2025-10-06 12:11:03 +0530

    remove Webdrivers

--- config/initializers/webdrivers.rb
+++ config/initializers/webdrivers.rb
@@ -1,5 +1,5 @@
-# config/initializers/webdrivers.rb
-require 'webdrivers/chromedriver'
+# # config/initializers/webdrivers.rb
+# require 'webdrivers/chromedriver'
 
-# Force webdrivers to use a specific Chromedriver version compatible with Chromium 140
-Webdrivers::Chromedriver.required_version = '140.0.7339.185'
+# # Force webdrivers to use a specific Chromedriver version compatible with Chromium 140
+# Webdrivers::Chromedriver.required_version = '140.0.7339.185'
